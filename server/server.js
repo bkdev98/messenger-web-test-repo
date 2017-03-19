dc39f0f040dbc76fd5becdf0aacc6accbf1e2d09
bkdev98 <bkdev98@gmail.com>
2017-03-19 10:10:34 +0700

    Completed chat event both server and client side

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 const bodyParser = require('body-parser');
 const path = require('path');
 const _ = require('lodash');
@@ -9,45 +11,37 @@ const MomentHandler = require("handlebars.moment");
 MomentHandler.registerHelpers(hbs);
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = process.env.PORT || 3001;
+
 app.use('/css', express.static(__dirname + '/../public/css'));
 app.use('/js', express.static(__dirname + '/../public/js'));
 app.use('/fonts', express.static(__dirname + '/../public/fonts'));
-app.use(cookieParser());
 
-hbs.registerPartials(__dirname + '/../views/partials');
-app.set('view engine', 'hbs');
+app.use(cookieParser());
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+hbs.registerPartials(__dirname + '/../views/partials');
+app.set('view engine', 'hbs');
+
 hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
     switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
         case '===':
             return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '!=':
-            return (v1 != v2) ? options.fn(this) : options.inverse(this);
         case '!==':
             return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
         default:
             return options.inverse(this);
     }
 });
+
+//  SocketIO
+
+//  Express Routers
 
 app.get('/', (req, res) => {
     axios.get('http://localhost:3000/api', {
@@ -63,13 +57,40 @@ app.get('/', (req, res) => {
 });
 
 app.get('/messengers/:id', (req, res) => {
+    //  GET data from API
     axios.get(`http://localhost:3000/api/messenger/${req.params.id}`, {
             headers: {
                 'x-auth': req.cookies.token
             }
         })
         .then((result) => {
-            // console.log(result.data);
+            io.on('connection', (socket) => {
+                console.log(`${result.data.currentUser.username} connected`);
+                //  Waiting for new message
+                socket.on('createMessage', (newEmail) => {
+                    //  POST data to API
+                    axios({
+                        method: 'post',
+                        url: `http://localhost:3000/api/messenger/${req.params.id}`,
+                        headers: {
+                            'x-auth': req.cookies.token
+                        },
+                        data: {
+                            'content': newEmail.content
+                        }
+                    }).then((mess) => {
+                        //  Emit event to clients
+                        socket.broadcast.emit('newMessage', mess.data);
+                    }).catch((e) => {
+                        console.log(e);
+                    });
+                });
+
+                socket.on('disconnect', () => {
+                    console.log('Disconnected to server');
+                });
+            });
+
             res.render('conversation.hbs', result.data);
         }).catch((e) => {
             // console.log(e);
@@ -126,6 +147,6 @@ app.get('/logout', (req, res) => {
     });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
