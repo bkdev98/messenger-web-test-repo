9751c798f95226f373d21d2cb04984c64b7dcf80
bkdev98 <bkdev98@gmail.com>
2017-03-22 19:28:33 +0700

    Refactor authentication using firebase

---
+++ public/js/firebase-config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyDt6RXCwFDaOiAT8QhZyU3iOKGrAiRPxNU",
+    authDomain: "messenger-app-6fb8e.firebaseapp.com",
+    databaseURL: "https://messenger-app-6fb8e.firebaseio.com",
+    storageBucket: "messenger-app-6fb8e.appspot.com",
+    messagingSenderId: "250437655467"
+};
+firebase.initializeApp(config);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const firebase = require('firebase');
 const socketIO = require('socket.io');
 const http = require('http');
 const bodyParser = require('body-parser');
@@ -11,6 +10,10 @@ const axios = require('axios');
 const MomentHandler = require("handlebars.moment");
 MomentHandler.registerHelpers(hbs);
 
+//  Firebase Configuration
+
+const firebase = require('firebase');
+
 const firebaseConfig = {
     apiKey: "AIzaSyDt6RXCwFDaOiAT8QhZyU3iOKGrAiRPxNU",
     authDomain: "messenger-app-6fb8e.firebaseapp.com",
@@ -21,6 +24,45 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+const messagesRef = firebase.database().ref("messages/");
+const usersRef = firebase.database().ref("users/");
+
+const middleware = {
+    authenticate: (req, res, next) => {
+        var token = req.cookies.token;
+        if (token) {
+            usersRef.orderByChild("idToken").equalTo(token).once("value", (snapshot) => {
+                req.currentUser = snapshot.val();
+                next();
+            }, (e) => {
+                res.redirect('/login');
+            })
+        } else {
+            res.redirect('/login');
+        }
+    },
+    info: (req, res, next) => {
+        var token = req.cookies.token;
+        if (token) {
+            usersRef.orderByChild("idToken").equalTo(token).once("value", (snapshot) => {
+                if (snapshot.val().fullname !== null) {
+                    next();
+                } else {
+                    res.redirect('/');
+                }
+            }, (e) => {
+                res.redirect('/login');
+            })
+        } else {
+            res.redirect('/login');
+        }
+    }
+}
+
+// messagesRef.set({
+//     content: "Test message"
+// });
+
 var API_URL = null;
 
 if (process.env.URL) {
@@ -28,8 +70,6 @@ if (process.env.URL) {
 } else {
     API_URL = 'http://localhost:3000/api'
 }
-
-console.log(process.env.URL, API_URL);
 
 const app = express();
 const server = http.createServer(app);
@@ -64,17 +104,38 @@ hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
 
 //  Express Routers
 
-app.get('/', (req, res) => {
-    axios.get(API_URL, {
-            headers: {
-                'x-auth': req.cookies.token
+app.get('/', [middleware.authenticate], (req, res) => {
+    // console.log("Current user: ", req.currentUser);
+    var userList = [];
+    usersRef.once('value', (snapshot) => {
+        Object.keys(snapshot.val()).map((key) => {
+            var { email, fullname } = snapshot.val()[key];
+            var user = {
+                email,
+                fullname,
+                uid: key
             }
-        })
-        .then((result) => {
-            res.render('index.hbs', result.data);
-        }).catch((e) => {
-            res.redirect('/login');
+            userList.push(user);
         });
+    })
+        .then(() => {
+            var filteredUserList = [];
+
+            filteredUserList = userList.filter((user) => {
+                return user.uid !== Object.keys(req.currentUser)[0]
+            });
+            // console.log('User List', userList);
+            // console.log('Filtered List', filteredUserList);
+            res.render('index.hbs', {
+                userList: filteredUserList,
+                title: "Messenger | Home"
+            });    
+        })
+        
+});
+
+app.get('/test', (req, res) => {
+    res.render('test.hbs');
 });
 
 var conversationId = null;
@@ -166,32 +227,40 @@ app.get('/register', (req, res) => {
     res.render('register.hbs');
 });
 
-app.post('/register', (req, res) => {
-    const { username, email, password } = req.body;
+app.get('/info', [middleware.authenticate, middleware.info], (req, res) => {
+    res.render('info.hbs', {
+        uid: Object.keys(req.currentUser)
+    });
+});
 
-    axios.post(`${API_URL}/auth/register`, { username, email, password })
-        .then((result) => {
-            res.cookie('token', result.headers['x-auth']);
-            res.redirect('/');
+app.post('/register', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        idToken: req.cookies.token,
+        fullname: null
+    }
+
+    usersRef.child(req.body.uid).set(newUser)
+        .then((user) => {
+            res.send({redirect: '/info'});
         })
-        .catch((err) => {
+        .catch((e) => {
             res.redirect('/register');
         });
+
+    // axios.post(`${API_URL}/auth/register`, { username, email, password })
+    //     .then((result) => {
+    //         res.cookie('token', result.headers['x-auth']);
+    //         res.redirect('/');
+    //     })
+    //     .catch((err) => {
+    //         res.redirect('/register');
+    //     });
 });
 
 app.get('/logout', (req, res) => {
     res.clearCookie('token');
-    axios.delete(`${API_URL}/auth/logout`, {
-        headers: {
-            'x-auth': req.cookies.token
-        }
-    })
-    .then(() => {
-        res.redirect('/login');
-    })
-    .catch((e) => {
-        res.redirect('/');
-    });
+    res.redirect('/login');
 });
 
 server.listen(port, () => {
