716b4595b1125d9369b8cd11b60acedd82f0901c
bkdev98 <bkdev98@gmail.com>
2017-03-23 01:02:46 +0700

    Refactor app using full firebase

--- server/server.js
+++ server/server.js
@@ -26,13 +26,40 @@ firebase.initializeApp(firebaseConfig);
 
 const messagesRef = firebase.database().ref("messages/");
 const usersRef = firebase.database().ref("users/");
+const conversationRef = firebase.database().ref("conversation/");
+
+// var newConver = {
+//     'xBgJudz05xTzqpoJw5JMuNbUqn42': 'dSp5eGLgUgbzdEDBybQaxaTO6bT2'
+// };
+
+// var newConverRef = conversationRef.push(newConver);
+// conversationId = newConverRef.key;
+// conversationRef.child(conversationId).set(newConver).then(() => {
+//     console.log("ss");
+// });
+
+// var newMess = {
+//     sender: "xBgJudz05xTzqpoJw5JMuNbUqn42",
+//     content: "hi",
+//     conversationId: "-KfqT0jbqOxPv0AYXelJ",
+//     createdAt: "123"
+// }
+
+// var newMessRef = messagesRef.push(newMess);
+// messagesRef.child(newMessRef.key).set(newMess);
 
 const middleware = {
     authenticate: (req, res, next) => {
         var token = req.cookies.token;
         if (token) {
             usersRef.orderByChild("idToken").equalTo(token).once("value", (snapshot) => {
-                req.currentUser = snapshot.val();
+                var uid = Object.keys(snapshot.val())[0];
+                user = {
+                    uid,
+                    email: snapshot.val()[uid].email,
+                    fullname: snapshot.val()[uid].fullname
+                }
+                req.currentUser = user;
                 next();
             }, (e) => {
                 res.redirect('/login');
@@ -43,12 +70,13 @@ const middleware = {
     },
     info: (req, res, next) => {
         var token = req.cookies.token;
+        // console.log(token);
         if (token) {
             usersRef.orderByChild("idToken").equalTo(token).once("value", (snapshot) => {
                 if (snapshot.val().fullname !== null) {
                     next();
                 } else {
-                    res.redirect('/');
+                    res.redirect('/info');
                 }
             }, (e) => {
                 res.redirect('/login');
@@ -56,6 +84,113 @@ const middleware = {
         } else {
             res.redirect('/login');
         }
+    },
+    gettargetuser: (req, res, next) => {
+        var targetUser = {};
+        var id = req.params.id;
+
+        usersRef.orderByKey().equalTo(id).once("value", (snapshot) => {
+            if (!snapshot) {
+                res.redirect('/');
+            } else {
+                req.targetUser = snapshot.val()[id];
+                next();
+            }
+        });
+    },
+    getuserlist: (req, res, next) => {
+        var userList = [];
+        usersRef.once('value', (snapshot) => {
+            Object.keys(snapshot.val()).map((key) => {
+                var { email, fullname } = snapshot.val()[key];
+                var user = {
+                    email,
+                    fullname,
+                    uid: key
+                }
+                userList.push(user);
+            });
+        })
+            .then(() => {
+                var filteredUserList = [];
+
+                filteredUserList = userList.filter((user) => {
+                    return user.uid !== req.currentUser.uid
+                });
+
+                req.userList = filteredUserList;
+                next();
+            })
+    },
+    getmessagelist: (req, res, next) => {
+        targetUserId = req.params.id;
+        currentUserId = req.currentUser.uid;
+        var conversationId = null;
+        var messageList = [];
+
+        conversationRef.orderByChild(currentUserId).equalTo(targetUserId).once("value", (snapshot) => {
+            if (snapshot.val() !== null) {
+                conversationId = Object.keys(snapshot.val())[0];
+                //  GET messages
+                return messagesRef.once('value').then((snapshot) => {
+                    var messages = snapshot.val() || {};
+                    
+                    Object.keys(messages).forEach((key) => {
+                        if (messages[key].conversationId === conversationId) {
+                            messageList.push(messages[key]);
+                        }
+                    });
+                    // console.log('mess 1', messageList);
+                    req.messageList = messageList;
+                    req.conversationId = conversationId;
+                    next();
+                });
+            } else {
+                conversationRef.orderByChild(targetUserId).equalTo(currentUserId).once("value", (snapshot) => {
+                    if (snapshot.val() !== null) {
+                        conversationId = Object.keys(snapshot.val())[0];
+                        // console.log('conver', conversationId);
+                        //  GET messages
+                        return messagesRef.once("value").then((snapshot) => {
+                            var messages = snapshot.val() || {};
+                            
+                            Object.keys(messages).forEach((key) => {
+                                if (messages[key].conversationId === conversationId) {
+                                    messageList.push(messages[key]);
+                                }
+                            });
+                            // console.log('mess 2', messageList);
+                            req.messageList = messageList;
+                            req.conversationId = conversationId;
+                            next();
+                        });
+                    } else {
+                        var newConver = {};
+                        newConver[currentUserId] = targetUserId;
+
+                        var newConverRef = conversationRef.push(newConver);
+                        conversationId = newConverRef.key;
+                        conversationRef.child(conversationId).set(newConver)
+                            .then(() => {
+                                return messagesRef.once("value").then((snapshot) => {
+                                    var messages = snapshot.val() || {};
+                                    
+                                    Object.keys(messages).forEach((key) => {
+                                        if (messages[key].conversationId === conversationId) {
+                                            messageList.push(messages[key]);
+                                        }
+                                    });
+                                    // console.log('mess 3', messageList);
+                                    req.messageList = messageList;
+                                    req.conversationId = conversationId;
+                                    next();
+                                });
+                            });
+                    }
+                });
+            }
+        });
+
     }
 }
 
@@ -104,106 +239,79 @@ hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
 
 //  Express Routers
 
-app.get('/', [middleware.authenticate], (req, res) => {
-    // console.log("Current user: ", req.currentUser);
-    var userList = [];
-    usersRef.once('value', (snapshot) => {
-        Object.keys(snapshot.val()).map((key) => {
-            var { email, fullname } = snapshot.val()[key];
-            var user = {
-                email,
-                fullname,
-                uid: key
-            }
-            userList.push(user);
-        });
-    })
-        .then(() => {
-            var filteredUserList = [];
-
-            filteredUserList = userList.filter((user) => {
-                return user.uid !== Object.keys(req.currentUser)[0]
-            });
-            // console.log('User List', userList);
-            // console.log('Filtered List', filteredUserList);
-            res.render('index.hbs', {
-                userList: filteredUserList,
-                title: "Messenger | Home"
-            });    
-        })
-        
+app.get('/', [middleware.authenticate, middleware.info, middleware.getuserlist], (req, res) => {
+    res.render('index.hbs', {
+        userList: req.userList,
+        title: "Messenger | Home"
+    }); 
 });
 
 app.get('/test', (req, res) => {
     res.render('test.hbs');
 });
 
-var conversationId = null;
-var targetUserId = null;
-var authToken = null;
+app.get('/messengers/:id', [middleware.authenticate, middleware.gettargetuser, middleware.getuserlist, middleware.getmessagelist], (req, res) => {
+    var conversationId = req.conversationId;
 
-app.get('/messengers/:id', (req, res) => {
-    targetUserId = req.params.id;
-    authToken = req.cookies.token;
-    //  GET data from API
-    axios.get(`${API_URL}/messenger/${req.params.id}`, {
-            headers: {
-                'x-auth': req.cookies.token
+    io.on('connection', (socket) => {
+        // console.log(`${result.data.currentUser.username} connected`);
+        //  Waiting for new message
+
+        socket.on('join', function (room) {
+
+            if (socket.room) {
+                console.log(req.currentUser.fullname + " left room" + socket.room);
+                socket.leave(socket.room);
             }
-        })
-        .then((result) => {
-            conversationId = result.data.conversationId;
 
-
-            io.on('connection', (socket) => {
-                // console.log(`${result.data.currentUser.username} connected`);
-                //  Waiting for new message
-
-                socket.on('join', function (room) {
-
-                    if (socket.room) {
-                        console.log(result.data.currentUser.username + " left room" + socket.room);
-                        socket.leave(socket.room);
-                    }
-
-                    socket.room = room;
-                    console.log(result.data.currentUser.username + " joined room" + conversationId);
-                    socket.join(room);
-                });
-
-                // console.log(socket.id);
-                socket.on('createMessage', (newEmail) => {
-                    //  POST data to API
-                    axios({
-                        method: 'post',
-                        url: `${API_URL}/messenger/${targetUserId}`,
-                        headers: {
-                            'x-auth': authToken
-                        },
-                        data: {
-                            'content': newEmail.content
-                        }
-                    }).then((mess) => {
-                        //  Emit event to clients
-                        socket.broadcast.to(conversationId).emit('newMessage', mess.data);
-                    }).catch((e) => {
-                        console.log(e);
-                    });
-                });
-
-                socket.on('disconnect', () => {
-                    console.log(result.data.currentUser.username + ' disconnected to server');
-                });
-            });
-
-
-
-
-            res.render('conversation.hbs', result.data);
-        }).catch((e) => {
-            // console.log(e);
-            res.redirect('/login');
+            socket.room = room;
+            console.log(req.currentUser.fullname + " joined room" + conversationId);
+            socket.join(room);
         });
+
+        // console.log(socket.id);
+        socket.on('createMessage', (newEmail) => {
+            var newMessage = {
+                sender: req.currentUser.fullname,
+                createdAt: new Date().getTime(),
+                content: newEmail.content,
+                conversationId
+            };
+            var newMessRef = messagesRef.push(newMessage);
+            messagesRef.child(newMessRef.key).set(newMessage);
+            messagesRef.on("child_added").then((snapshot) => {
+                console.log('success', snapshot.key);
+            });
+        });
+
+        socket.on('disconnect', () => {
+            console.log(req.currentUser.fullname + ' disconnected to server');
+        });
+    });
+
+
+    res.render('conversation.hbs', {
+        messages: req.messageList,
+        userList: req.userList,
+        targetUser: req.targetUser,
+        currentUser: req.currentUser,
+        conversationId
+    });
+
+    // //  GET data from API
+    // axios.get(`${API_URL}/messenger/${req.params.id}`, {
+    //         headers: {
+    //             'x-auth': req.cookies.token
+    //         }
+    //     })
+    //     .then((result) => {
+    //         conversationId = result.data.conversationId;
+
+    //         res.render('conversation.hbs', result.data);
+    //     }).catch((e) => {
+    //         // console.log(e);
+    //         res.redirect('/login');
+    //     });
 });
 
 app.get('/login', (req, res) => {
@@ -211,16 +319,15 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const { email, password } = req.body;
+    const { uid, idToken } = req.body;
 
-    axios.post(`${API_URL}/auth/login`, { email, password })
-        .then((result) => {
-            res.cookie('token', result.headers['x-auth']);
-            res.redirect('/');
+    usersRef.child(uid).update({ idToken })
+        .then((user) => {
+            res.send({redirect: '/'});
         })
-        .catch((err) => {
+        .catch((e) => {
             res.redirect('/login');
-        });
+        })
 })
 
 app.get('/register', (req, res) => {
@@ -229,7 +336,7 @@ app.get('/register', (req, res) => {
 
 app.get('/info', [middleware.authenticate, middleware.info], (req, res) => {
     res.render('info.hbs', {
-        uid: Object.keys(req.currentUser)
+        uid: req.currentUser.uid
     });
 });
 
@@ -240,22 +347,22 @@ app.post('/register', (req, res) => {
         fullname: null
     }
 
-    usersRef.child(req.body.uid).set(newUser)
-        .then((user) => {
-            res.send({redirect: '/info'});
-        })
-        .catch((e) => {
-            res.redirect('/register');
-        });
-
-    // axios.post(`${API_URL}/auth/register`, { username, email, password })
-    //     .then((result) => {
-    //         res.cookie('token', result.headers['x-auth']);
-    //         res.redirect('/');
-    //     })
-    //     .catch((err) => {
-    //         res.redirect('/register');
-    //     });
+    usersRef.child(req.body.uid).once("value", (snapshot) => {
+        if(snapshot.val()) {
+            usersRef.child(req.body.uid).update({idToken: req.cookies.token})
+                .then(() => {
+                    res.send({redirect: '/'});
+                });
+        } else {
+            usersRef.child(req.body.uid).set(newUser)
+                .then((user) => {
+                    res.send({redirect: '/info'});
+                })
+                .catch((e) => {
+                    res.redirect('/register');
+                });
+        }
+    });
 });
 
 app.get('/logout', (req, res) => {
